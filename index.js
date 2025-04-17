8d988f068ab591ce7d64b3ebb9aa2cf19f44840d
maria-martins004 <martins.maedu@gmail.com>
2025-04-17 14:53:40 -0300

    Alteração Responsividade para celulares iniciada

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ tabMenu.forEach((itemMenu, index) => {
     activeTab(index);
   })
 });
+
+const container = document.querySelector('.barbie');
+const mediaQuery = window.matchMedia('(max-width: 615px)');
+
+function Responsividade(r) {
+  if (r.matches) {
+    container.style.flexDirection = 'column';
+  } else {
+    container.style.flexDirection = 'row';
+  }
+}
+
+Responsividade(mediaQuery);
+
+mediaQuery.addEventListener('change', Responsividade);
